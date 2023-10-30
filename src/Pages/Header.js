9658596fd7621da8be56af55d76ac00058a0d0f8
Reviver <108503367+Reviver123456@users.noreply.github.com>
2023-10-30 16:11:46 +0700

    แก้ ทีม halloween

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -6,7 +6,7 @@ import { Container, Row, Col, Tab, Tabs } from "react-bootstrap";
 
 import BgheaderPc from "../Images/Pc/ImgPc24.png";
 import BgiconPc from "../Images/Pc/ImgPc25.png";
-import IconLsmPc from "../Images/Pc/ImgPc6.png";
+import IconLsmPc from "../Images/Pc/ImgPc6.gif";
 
 import IconwalletPc from "../Images/Pc/ImgPc7.png";
 import IcondepositPc from "../Images/Pc/ImgPc8.png";
@@ -23,7 +23,7 @@ import IconregisterPc from "../Images/Pc/ImgPc5.png";
 
 /*mobie */
 import BgheaderMb from "../Images/Mb/ImgMb21.png";
-import IconLsmMb from "../Images/Mb/ImgMb6.png";
+import IconLsmMb from "../Images/Mb/ImgMb6-.gif";
 import BgfootterMb from "../Images/Mb/ImgMb22.png";
 
 import IconwalletMb from "../Images/Mb/ImgMb7.png";
