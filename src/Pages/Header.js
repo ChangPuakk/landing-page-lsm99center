7c8e41785795714e042eba6196a5dd06c070de97
Reviver <hero_kong@live.com>
2024-10-09 14:24:11 +0700

    Update Header.js

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -7,7 +7,7 @@ import titlelogo from "../Images/new-center/logo.png";
 export default function Header() {
   const [linkLogin, setLinkLogin] = useState("https://lsm99center.l3t.lsmplay.com/login");
   const [linkRegister, setRegister] = useState(
-    "https://lsm99center.l3t.lsmplay.com/register"
+    "https://lsm99center.l3t.lsmplay.com/register?partner=904&channel=xbgcen"
   );
   return (
     <>
